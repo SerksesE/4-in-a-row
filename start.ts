524b4ab2fc710d5be0887058b857de98500ccd57
Serkses <serkses@madx.nl>
2019-08-23 16:08:51 +0200

    validate board and fill

--- start.ts
+++ start.ts
@@ -1,14 +1,37 @@
 import * as readline from 'readline'
 
 async function main() {
+  let board = [
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+  ]
+
 
   while (true) {
     const ans = await askQuestion("Which column number? ");
 
     if (parseInt(ans) > 0 && parseInt(ans) < 8) {
       console.log("Your answer:", ans)
+
+      for (let i = 0; i < board[0].length - 1; i++) {
+        if (board[i][parseInt(ans) - 1] !== '-') {
+          board[i - 1][parseInt(ans) - 1] = 'o'
+        }
+        if (i === board.length - 1) {
+          if (board[i][parseInt(ans) - 1] !== '-') {
+            board[i - 1][parseInt(ans) - 1] = 'o'
+          } else {
+            board[i][parseInt(ans) - 1] = 'o'
+          }
+        }
+      }
+      console.log(board)
     } else {
-      console.log("Invalid column! Choose a number between 1 - 7")
+      console.log("Invalid column number! Choose a number between 1 - 7")
     }
   }
 }
