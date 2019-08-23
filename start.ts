4530714261d7d594d2cd92f03391a6d8adc08425
Serkses <serkses@madx.nl>
2019-08-23 15:12:05 +0200

    validate input

--- start.ts
+++ start.ts
@@ -1,24 +1,29 @@
 import * as readline from 'readline'
-​
+
 async function main() {
-​
+
   while (true) {
     const ans = await askQuestion("Which column number? ");
-    console.log("Your answer:", ans)
+
+    if (parseInt(ans) > 0 && parseInt(ans) < 8) {
+      console.log("Your answer:", ans)
+    } else {
+      console.log("Invalid column! Choose a number between 1 - 7")
+    }
   }
 }
-​
+
 function askQuestion(query: string) {
-​
+
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   })
-​
+
   return new Promise<string>(resolve => rl.question(query, ans => {
     rl.close()
     resolve(ans)
   }))
 }
-​
+
 main().catch(console.error)
